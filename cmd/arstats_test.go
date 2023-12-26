41db1fcd691fc08d286551d05b40fd00d173e082
Phil Hanna <ph1204@gmail.com>
2023-12-26 11:50:44 -0500

    Remove trailing spaces

--- cmd/arstats_test.go
+++ cmd/arstats_test.go
@@ -1,26 +1,23 @@
 package main
 
 import (
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_padParts(t *testing.T) {
-	type args struct {
-		parts []string
-	}
+
 	tests := []struct {
 		name string
-		args args
+		parts []string
 		want []string
 	}{
-		{"Simple", args{[]string{"Larry", "Curly", "Moe"}}, []string{"Larry", "Curly", "Moe  "}},
+		{"Simple", []string{"Larry", "Curly", "Moe"}, []string{"Larry", "Curly", "Moe"}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := padParts(tt.args.parts); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("padParts() = %v, want %v", got, tt.want)
-			}
+			assert.Equal(t, tt.want, tt.parts)
 		})
 	}
 }
